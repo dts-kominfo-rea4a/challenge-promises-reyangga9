8115868da30cd6cef098077485d687df8f73851a
reyangga9 <reyangga5@gmail.com>
2022-09-19 22:42:14 +0700

    tugaszz

--- promise.js
+++ promise.js
@@ -1,7 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  let hasil =0;
+
+  return new Promise (async(resolve)=>{
+    let TheaterIXX = await promiseTheaterIXX()
+    .then(function (data) {
+      data.forEach(data => {
+        if(data.hasil === emosi){
+          hasil++;
+        }
+      })
+    });
+    let theaterVGC = await promiseTheaterVGC()
+    .then(function (data){
+      data.forEach(function (data) {
+        if(data.hasil === emosi){
+          hasil++
+        }
+      })
+    });
+  resolve(hasil);
+  })
+};
+
+
 
 module.exports = {
   promiseOutput,
